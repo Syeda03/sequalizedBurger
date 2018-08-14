7a0d57f46f6002209c05ca2e57d600aa957ccec1
syeda zehra <zehrasyeda@gmail.com>
2018-08-14 18:24:19 -0500

    final

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burgers');
+INSERT INTO burgers (burger_name) VALUES (' CHEESEBURGER WITH GRILLED ONIONS');
+INSERT INTO burgers (burger_name) VALUES ('Mushroom BURGER');
